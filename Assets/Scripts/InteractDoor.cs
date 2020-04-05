4a6c07ac7ccd00d44988b791e4d881fb60671487
sim2kid <sim2kid@users.noreply.github.com>
2020-04-04 19:57:57 -0500

    Added a Door with animations

---
+++ Assets/Scripts/InteractDoor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractDoor : MonoBehaviour, IInteract
+{
+    [Tooltip("What shows up when you hover over the object")] [SerializeField] private string tooltip = "Huh... This hasn't been filled in... Oops!!";
+    private Animator anime;
+
+    private void Start()
+    {
+        anime = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+
+    }
+
+    public void interact()
+    {
+        anime.SetTrigger("OpenClose");
+    }
+
+    public string getTooltip()
+    {
+        return tooltip;
+    }
+}
